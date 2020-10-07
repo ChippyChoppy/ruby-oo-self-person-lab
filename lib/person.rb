5d57c00d93fff99d325c3f55346dc97e919569ef
ChippyChoppy <rebeccamargaret13@gmail.com>
2020-10-07 11:39:35 -0400

    pairing lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,33 @@
 # your code goes here
+require 'pry'
+
+class Person
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name     
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+#binding.pry 
+    def happiness
+        @happiness = 0 if @happiness < 0 
+        @happiness = 10 if @happiness > 10
+        @happiness  
+    end       
+    
+    def hygiene 
+        @hygiene = 0 if @hygiene < 0
+        @hygiene = 10 if @hygiene > 10
+        @hygiene
+    end
+
+    
+
+        #binding.pry
+        #else
+         #   @happiness 
+    #binding.pry
+end
